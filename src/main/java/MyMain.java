7dd0b300c04a8cca15909c09600f7850ecf94ded
Sentientnapkin <24svargas@athenian.org>
2021-09-01 14:33:11 -0700

    No need to answer every question

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,11 +1,23 @@
 import java.util.Scanner;
-
+//scan.next() for next word, scan.nextLine
 public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // Add some code here!
-
+        System.out.println("What is your name?");
+        String name = scan.nextLine();
+        System.out.println("Good Morning "+ name);
+        System.out.println("What year were you born in?");
+        String age = scan.nextLine();
+        System.out.println(age + " really was a good year wasn't it!");
+        System.out.println("What is your social security number?");
+        String ssnum = scan.nextLine();
+        System.out.println(ssnum+" really rolls right off the tongue.");
+        System.out.println("Where were you born?");
+        String birthplace = scan.nextLine();
+        System.out.println("I personally love " + birthplace);
+        System.out.println("And yoru favorite color?");
+        String favcolor = scan.nextLine();
+        System.out.println("I personally dislike "+favcolor);
         scan.close();
     }
 }
